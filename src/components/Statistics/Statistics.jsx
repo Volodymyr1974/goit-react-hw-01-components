640c3e289dbd32297f6b2f1501104cbedd2b7576
Volodymyr Zvarych <Vbzvar@gmail.com>
2022-07-04 00:32:10 +0300

    виправляє 2 завдання

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,25 +1,25 @@
 import getRandomHexColor from './color.js';
-import s from './Statistics.module.css';
+import style from './Statistics.module.css';
 import PropTypes from 'prop-types';
 function Statistics({
     title,
     stats,
 }) {
     return (
-    <section className={s.statistics}>
-      {title && <h2 className={s.title}>{title}</h2>}
+    <section className={style.statistics}>
+      {title && <h2 className={style.title}>{title}</h2>}
 
-      <ul className={s.statList}>
+      <ul className={style.statList}>
         {stats.map((stat) => (
           <li
             key={stat.id}
-            className={s.item}
+            className={style.item}
             style={{
               backgroundColor: getRandomHexColor(),
             }}
           >
-            <span className={s.label}>{stat.label}</span>
-            <span className={s.percentage}>{stat.percentage}%</span>
+            <span className={style.label}>{stat.label}</span>
+            <span className={style.percentage}>{stat.percentage}%</span>
           </li>
         ))}
       </ul>
