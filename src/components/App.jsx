cda5fc94bb5ee8c1bb0a24abbe87a8da12c9920c
Volodymyr Zvarych <Vbzvar@gmail.com>
2022-07-04 00:28:11 +0300

    додає 1-3 завдання

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import FriendList from './FriendList/FriendList';
+import user from '../data/user.json';
+import data from '../data/data.json';
+import friends from '../data/friends.json';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div    
+    >      
+      <Profile
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+        username={user.username} />
+      <Statistics title="Upload stats" stats={data} />
+<FriendList friends={friends} />
     </div>
   );
 };
---
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,23 @@
+import style from './FriendListItem.module.css';
+import PropTypes from 'prop-types';
+
+function FriendListItem({
+    avatar,
+    name,
+    isOnline }) {
+  return (
+    <li className={style.item}>
+      <span className={`${style.status} ${isOnline && style.online} `}></span>
+      <img className={style.avatar} src={avatar} alt="User avatar" width="48" />
+      <p className={style.name}>{name}</p>
+    </li>
+  );
+};
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
+
+export default FriendListItem;
